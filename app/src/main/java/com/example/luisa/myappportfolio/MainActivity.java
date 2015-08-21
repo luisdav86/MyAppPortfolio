6c1a48f647b638bdb5cf2abbcbe6e90d9e0cbde9
luisdav86@gmail.com <luisdav86@gmail.com>
2015-08-21 16:38:24 -0600

    adding main activity

--- app/src/main/java/com/example/luisa/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/luisa/myappportfolio/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.luisa.myappportfolio;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,25 +17,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onClickButton(View v) {
+        int textResourceId = -1;
+        switch (v.getId()) {
+            case R.id.media_streamer:
+                textResourceId = R.string.main_activity_spotify_app_name;
+                break;
+            case R.id.super_do1:
+                textResourceId = R.string.main_activity_super_dou_app_name;
+                break;
+            case R.id.super_do2:
+                textResourceId = R.string.main_activity_build_app_name;
+                break;
+            case R.id.ant_terminator:
+                textResourceId = R.string.main_activity_build_app_name;
+                break;
+            case R.id.materialize:
+                textResourceId = R.string.main_activity_xyz_app_name;
+                break;
+            case R.id.capstone:
+                textResourceId = R.string.main_activity_capstone_app_name;
+                break;
+            default:
+                break;
+        }
+        Resources resources = getResources();
+        showToast(resources.getString(R.string.main_activity_button_toast_description) + " " + getResources().getString(textResourceId));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void showToast(String text) {
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
     }
 }
